21e507f3a4da1d8d49c85cba2781b2bd4160996d
muzi470-lgtm <muzammiln411@gmail.com>
2026-05-21 08:02:32 +0500

    sitemap2

--- src/app/robots.js
+++ src/app/robots.js
@@ -8,15 +8,6 @@ export default function robots() {
         allow: '/',
         disallow: ['/api/', '/_next/', '/static/'],
       },
-      {
-        userAgent: 'Googlebot',
-        allow: '/',
-        disallow: ['/api/'],
-      },
-      {
-        userAgent: 'Googlebot-Image',
-        allow: '/',
-      },
     ],
     sitemap: 'https://freelancefeecalc.site/sitemap.xml',
     host: 'freelancefeecalc.site',
